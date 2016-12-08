faed5de345182d9cd3a519828505549b3326b4b0
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2016-12-08 23:38:44 +0100

    adding components and material

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,26 +1,29 @@
 import React, { Component, PropTypes } from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class AppContainer extends Component {
   static propTypes = {
-    routes : PropTypes.object.isRequired,
-    store  : PropTypes.object.isRequired
+    routes: PropTypes.object.isRequired,
+    store: PropTypes.object.isRequired
   }
 
-  shouldComponentUpdate () {
+  shouldComponentUpdate() {
     return false
   }
 
-  render () {
+  render() {
     const { routes, store } = this.props
 
     return (
-      <Provider store={store}>
-        <div style={{ height: '100%' }}>
-          <Router history={browserHistory} children={routes} />
-        </div>
-      </Provider>
+      <MuiThemeProvider>
+        <Provider store={store}>
+          <div>
+            <Router history={browserHistory} children={routes}/>
+          </div>
+        </Provider>
+      </MuiThemeProvider>
     )
   }
 }
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import { Link } from 'react-router'
 import './HomeView.scss'
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
+  <div className="container-fluid text-center">
+    <div className="row">
+      <h4>Welcome To the feedback configurator</h4>
+    </div>
+
+    <Link to='/counter' className="row" activeClassName='route--active'>
+      Load a merchant by ID
+    </Link>
+    <Link to='/counter' activeClassName='route--active'>
+      Login and pick a merchant
+    </Link>
+    <RaisedButton label="Default" />
   </div>
 )
 
